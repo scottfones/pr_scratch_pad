86452a6c307fb1997d97f5a1beb804894dc27c5d
Scott Fones <scott.fones@gmail.com>
2021-12-01 18:53:28 -0500

    added chapter 10

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod early_chapters;
 mod chapter_eight;
 mod chapter_nine;
+mod chapter_ten;
 
 struct Point {
     x: i32,
@@ -9,5 +10,6 @@ struct Point {
 fn main() {
     //early_chapters::early_chapters_main();
     //chapter_eight::chapter_eight_main();
-    chapter_nine::chapter_nine_main();
+    //chapter_nine::chapter_nine_main();
+    chapter_ten::chapter_ten_main();
 }
